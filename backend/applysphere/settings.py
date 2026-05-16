392b08f740d7deaff4e1e042fa52c18824d5e496
Ganpat <pganpatchoudhary@gmail.com>
2026-05-16 21:25:18 +0530

    Change the secret key and secured it using .env

--- backend/applysphere/settings.py
+++ backend/applysphere/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-cur%ge3ndom150knt!e@32m4385w*5639gsghz@wq#&smq0bad"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
