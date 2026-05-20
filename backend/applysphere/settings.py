a0a3a78d964d855d4e908f84a1dd5c51f09a46c3
Ganpat <pganpatchoudhary@gmail.com>
2026-05-20 20:45:59 +0530

    Restrict production CORS and hosts

--- backend/applysphere/settings.py
+++ backend/applysphere/settings.py
@@ -31,14 +31,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-# ALLOWED_HOSTS = [
-#     "localhost",
-#     "127.0.0.1",
-#     "applyhub-fa3y.onrender.com",
-# ]
-
 ALLOWED_HOSTS = [
-    "*"
+    "localhost",
+    "127.0.0.1",
+    "applyhub-fa3y.onrender.com",
 ]
 
 CORS_ALLOWED_ORIGINS = [
